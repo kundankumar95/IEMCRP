c68e2dfbefcf264260a5fac94f03e8c1ae8299e6
kundankumar95 <kundanburnwal32434@gmail.com>
2024-03-28 15:02:16 +0530

    change commit

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ function ShowCurrentDate() {
 setInterval(ShowTime, 1000);
 ShowCurrentDate();
 
+
+// student page
+document.getElementById('user-input').addEventListener('click', function() {
+    const unameValue = document.getElementById('Username').value;
+    const passwordValue = document.getElementById('password').value;
+    const dropdownValue = document.getElementById('dropdown').value;
+
+    // Check for correct credentials and dropdown selection
+    if (unameValue === "12022002023213" && passwordValue === "1234" && dropdownValue === "Student") {
+        // Redirect to the student.html page
+        window.location.href = "student.html";
+    } else {
+        alert("Incorrect username, password, or user type.");
+    }
+});
+
+
+const option_btn = document.getElementById('option');
+
+
+
+
